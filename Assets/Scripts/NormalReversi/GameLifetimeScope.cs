fd49b0515f36faa87d8a8d18e80be4c53ef7807d
eyedrops <eyedropsp@gmail.com>
2021-09-29 22:57:03 +0900

    GameLifetimeScope追加

---
+++ Assets/Scripts/NormalReversi/GameLifetimeScope.cs
@@ -0,0 +1,27 @@
+using NormalReversi.Models;
+using NormalReversi.Models.Interface;
+using NormalReversi.Models.Manager;
+using NormalReversi.Presenter;
+using NormalReversi.View;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace NormalReversi
+{
+    public class GameLifetimeScope : LifetimeScope
+    {
+        [SerializeField] private GridManager _gridManager;
+        
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterEntryPoint<ReversiPresenter>();
+            builder.Register<IGameManager, GameManager>(Lifetime.Singleton);
+            builder.Register<IPlayer, Player>(Lifetime.Singleton);
+            builder.RegisterComponent(_gridManager)
+                .AsImplementedInterfaces();
+            builder.RegisterComponentInHierarchy<ReversiView>();
+            builder.RegisterComponentInHierarchy<ReversiGUI>();
+        }
+    }
+}

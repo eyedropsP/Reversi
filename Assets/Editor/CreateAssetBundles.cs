24c69c359ac9ede3953fe8185e3a02477e1c6003
eyedrops <eyedropsp@gmail.com>
2021-10-04 21:11:53 +0900

    Addressable, AssetBundleを追加してみた

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Editor
+{
+    public class CreateAssetBundles
+    {
+        [MenuItem("Assets/Build AssetBundles")]
+        static void BuildAssetBundles()
+        {
+            var assetBundleDirectory = "Assets/AssetBundles";
+            if (!Directory.Exists(assetBundleDirectory))
+            {
+                Directory.CreateDirectory(assetBundleDirectory);
+            }
+
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+                BuildTarget.StandaloneWindows);
+        }
+    }
+}
